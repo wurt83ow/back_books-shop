be16efbc5d9320f411836f6b9f0e18707d55f204
verter187 <verter187@inbox.ru>
2022-06-19 18:16:12 +0300

    back minor improvements 20020619

--- controllers/Controller.js
+++ controllers/Controller.js
@@ -3,6 +3,7 @@ const { Book } = require("../models/models");
 class Controllers {
   async getBooks(req, res) {
     const books = await Book.findAll();
+    console.log(books);
     return res.send(books);
   }
 }
--- models/models.js
+++ models/models.js
@@ -5,9 +5,11 @@ const { INTEGER, STRING } = DataTypes;
 
 const Book = sequelize.define("Book", {
   id: { type: INTEGER, primaryKey: true, autoIncrement: true },
-  name: { type: STRING, allowNull: false },
-  price: { type: INTEGER, allowNull: false },
-  comment: { type: STRING, allowNull: false },
+  title: { type: STRING, allowNull: false },
+  price: { type: STRING, allowNull: false },
+  descr: { type: STRING, allowNull: false },
+  type: { type: STRING, allowNull: false },
+  img_name: { type: STRING, allowNull: false },
 });
 
 module.exports = { Book };
